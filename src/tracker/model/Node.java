737f7762951b34d4c2db60ab1a752744599ce269
Mihey <miheyzhukov@yandex.ru>
2024-06-23 02:04:15 +0300

    обновлен класс Node

--- src/tracker/model/Node.java
+++ src/tracker/model/Node.java
@@ -1,15 +1,52 @@
 package tracker.model;
 
+import java.util.Objects;
+
 public class Node<T> {
 
-        public T task;
-        public Node<T> next;
-        public Node<T> prev;
+    private final T task;
+    private Node<T> next;
+    private Node<T> prev;
 
-        public Node(Node<T> prev, T task, Node<T> next) {
-            this.task = task;
-            this.next = next;
-            this.prev = prev;
-        }
+    public Node(Node<T> prev, T task, Node<T> next) {
+        this.task = task;
+        this.next = next;
+        this.prev = prev;
+    }
 
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public T getTask() {
+        return task;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(task, node.task) && Objects.equals(next, node.next) && Objects.equals(prev, node.prev);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(task, next, prev);
+    }
 }
+
+
+
