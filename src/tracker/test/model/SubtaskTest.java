ae51f4026367f46742f3e8cc23ed35db8848c4ff
Mihey <miheyzhukov@yandex.ru>
2024-06-23 03:54:41 +0300

    исправление codeStyle

--- src/tracker/test/model/SubtaskTest.java
+++ src/tracker/test/model/SubtaskTest.java
@@ -6,8 +6,7 @@ import tracker.model.Subtask;
 
 class SubtaskTest {
     @Test
-    void testEqualsSubtask(){
-
+    void testEqualsSubtask() {
         Subtask subtask1 = new Subtask("Test1 title", "Test1 description",1);
         Subtask subtask2 = new Subtask("Test1 title", "Test1 description",1);
 
